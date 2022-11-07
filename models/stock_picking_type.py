883b1f1b9a62a7ef17c7b61b5fb5d3e9fdc1c652
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-11-07 13:59:38 +0100

    [FIX] Fix default value in <display_in_dashboard> and correct translation.

--- models/stock_picking_type.py
+++ models/stock_picking_type.py
@@ -7,6 +7,6 @@ from odoo.exceptions import UserError
 class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
 
-    display_in_dashboard = fields.Boolean(string='Display in main dashboard',default=False)
+    display_in_dashboard = fields.Boolean(string='Display in main dashboard',default=True)
 
 
